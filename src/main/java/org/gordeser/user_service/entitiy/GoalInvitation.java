117aab58a2faf5839222b375573fd0108db3cb14
Sergei Gordeev <gordeser@fit.cvut.cz>
2025-02-09 13:15:00 +0100

    fix: add id annotation to id field

--- src/main/java/org/gordeser/user_service/entitiy/GoalInvitation.java
+++ src/main/java/org/gordeser/user_service/entitiy/GoalInvitation.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -30,6 +32,8 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "goal_invitation")
 public class GoalInvitation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
