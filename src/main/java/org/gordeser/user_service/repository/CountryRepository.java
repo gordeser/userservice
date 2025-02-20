f1eb59467728206139c2abe7461668aeb2d3a2ab
Sergei Gordeev <gordeser@fit.cvut.cz>
2025-02-20 21:23:38 +0100

    feature: add findByName in CountryRepository

--- src/main/java/org/gordeser/user_service/repository/CountryRepository.java
+++ src/main/java/org/gordeser/user_service/repository/CountryRepository.java
@@ -4,6 +4,9 @@ import org.gordeser.user_service.entitiy.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
+    Optional<Country> findByName(String name);
 }
