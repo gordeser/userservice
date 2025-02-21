f50d00045dd75c89872ffa5abf100e3f1ad02bda
Sergei Gordeev <gordeser@fit.cvut.cz>
2025-02-21 22:14:07 +0100

    feature: add contact preference entity

---
+++ src/main/java/org/gordeser/user_service/entitiy/ContactPreference.java
@@ -0,0 +1,40 @@
+package org.gordeser.user_service.entitiy;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "contact_preference")
+public class ContactPreference {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "preference", nullable = false)
+    private PreferredContact preference;
+}
