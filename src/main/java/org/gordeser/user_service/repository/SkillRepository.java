49cdbfeb6dd4cc1b9578195fd0c8f71d87ef9666
Sergei Gordeev <gordeser@fit.cvut.cz>
2025-02-21 23:27:16 +0100

    feature: use existsById instead of findById

--- src/main/java/org/gordeser/user_service/repository/SkillRepository.java
+++ src/main/java/org/gordeser/user_service/repository/SkillRepository.java
@@ -4,9 +4,7 @@ import org.gordeser.user_service.entitiy.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Long> {
-    Optional<Skill> findByTitle(String title);
+    Boolean existsByTitle(String title);
 }
