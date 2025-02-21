2c6228ac14031f70df4775a5c15cdbbebdefbead
Sergei Gordeev <gordeser@fit.cvut.cz>
2025-02-21 22:12:50 +0100

    feature: add skill request entity

---
+++ src/main/java/org/gordeser/user_service/entitiy/SkillRequest.java
@@ -0,0 +1,37 @@
+package org.gordeser.user_service.entitiy;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "skill_request")
+public class SkillRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id", nullable = false)
+    private RecommendationRequest request;
+
+    @ManyToOne
+    @JoinColumn(name = "skill_id", nullable = false)
+    private Skill skill;
+}
