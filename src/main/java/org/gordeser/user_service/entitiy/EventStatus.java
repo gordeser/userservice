5e53970c16f8d9d5061b43c48e9348876387fcfd
Sergei Gordeev <gordeser@fit.cvut.cz>
2025-02-21 22:16:17 +0100

    feature: update event status enum

--- src/main/java/org/gordeser/user_service/entitiy/EventStatus.java
+++ src/main/java/org/gordeser/user_service/entitiy/EventStatus.java
@@ -1,16 +1,18 @@
 package org.gordeser.user_service.entitiy;
 
-import lombok.Getter;
-
-@Getter
 public enum EventStatus {
     PLANNED("Planned"),
     IN_PROGRESS("In progress"),
     CANCELLED("Cancelled"),
     COMPLETED("Completed");
+
     private final String status;
 
     EventStatus(String status) {
         this.status = status;
+    }
+
+    public String getMessage() {
+        return status;
     }
 }
