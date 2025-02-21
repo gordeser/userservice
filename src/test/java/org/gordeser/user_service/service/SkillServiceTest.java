c717b5d021da96042d0df50eaeefd4123c1673b6
Sergei Gordeev <gordeser@fit.cvut.cz>
2025-02-22 00:02:48 +0100

    fix: update createSkill service tests

--- src/test/java/org/gordeser/user_service/service/SkillServiceTest.java
+++ src/test/java/org/gordeser/user_service/service/SkillServiceTest.java
@@ -13,8 +13,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
 
 @ExtendWith(MockitoExtension.class)
 class SkillServiceTest {
@@ -22,10 +20,8 @@ class SkillServiceTest {
     @Mock
     private SkillRepository skillRepository;
 
-
     @InjectMocks
     private SkillService skillService;
-
 
     private Skill skill;
     private SkillDto skillDto;
@@ -39,29 +35,29 @@ class SkillServiceTest {
     @Test
     void createSkillTest() {
         // Arrange
-        Mockito.when(skillRepository.findByTitle(skillDto.getTitle())).thenReturn(Optional.empty());
+        Mockito.when(skillRepository.existsByTitle(skillDto.getTitle())).thenReturn(Boolean.FALSE);
         Mockito.when(skillRepository.save(Mockito.any(Skill.class))).thenReturn(skill);
 
         // Act
-        Skill createdSkill = skillService.createSkill(skillDto);
+        SkillDto createdSkill = skillService.createSkill(skillDto);
 
         // Assert
         Assertions.assertNotNull(createdSkill);
         Assertions.assertEquals(skillDto.getTitle(), createdSkill.getTitle());
-        Mockito.verify(skillRepository, Mockito.times(1)).findByTitle(skillDto.getTitle());
+        Mockito.verify(skillRepository, Mockito.times(1)).existsByTitle(skillDto.getTitle());
         Mockito.verify(skillRepository, Mockito.times(1)).save(Mockito.any(Skill.class));
     }
 
     @Test
     void createSkillTitleAlreadyExistsTest() {
         // Arrange
-        Skill anotherSkillWithTitle = Skill.builder().id(2L).title("TestSkill").build();
-        Mockito.when(skillRepository.findByTitle(skillDto.getTitle())).thenReturn(Optional.of(anotherSkillWithTitle));
+        Mockito.when(skillRepository.existsByTitle(skillDto.getTitle())).thenReturn(Boolean.TRUE);
 
         // Act & Assert
-        var exception = Assertions.assertThrows(DataValidationException.class, () -> skillService.createSkill(skillDto));
-        Assertions.assertEquals("Skill with that title already exists.", exception.getMessage());
-        Mockito.verify(skillRepository, Mockito.times(1)).findByTitle(skillDto.getTitle());
+        Exception exception = Assertions.assertThrows(DataValidationException.class, () -> skillService.createSkill(skillDto));
+
+        Assertions.assertEquals("Skill with title " + skillDto.getTitle() + " already exists.", exception.getMessage());
+        Mockito.verify(skillRepository, Mockito.times(1)).existsByTitle(skillDto.getTitle());
         Mockito.verify(skillRepository, Mockito.never()).save(Mockito.any(Skill.class));
     }
 }
